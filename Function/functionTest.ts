e3f49299346c3f7899caaeceb9b463398303aadc
ff605732747 <fwq605732747@live.cn>
2017-02-21 12:24:45 +0800

    泛型

--- Function/functionTest.ts
+++ Function/functionTest.ts
@@ -5,3 +5,28 @@ getName = function (name: string) {
 
     return `我的名字是 ${name}`
 }
+
+var getName: (name: string) => string = function (name) {
+    return `我的名字是 ${name}`
+}
+
+
+//有默认参数的函数
+function add(foo: number = 0, foobar: number, bar?: number): number {
+
+    return foo;
+};
+
+//有剩余参数的函数
+// function add2(...foo: number[]) { }
+function add2(foo: number[]) { }
+
+
+//函数重载 
+function test(value: (string | number | boolean)): string {
+
+
+    return `${value}`;
+}
+
+
