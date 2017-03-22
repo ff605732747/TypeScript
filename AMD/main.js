37c593f6a7e0a75914714ac9885cea7785b84926
ff605732747 <fwq605732747@live.cn>
2017-03-22 21:02:00 +0800

    ？

--- AMD/main.js
+++
@@ -1,7 +0,0 @@
-define(["require", "exports", "./test/greeter"], function (require, exports, model) {
-    "use strict";
-    exports.__esModule = true;
-    var el = document.getElementById('main');
-    var greeter = new model.Greeter(el);
-    greeter.start();
-});
